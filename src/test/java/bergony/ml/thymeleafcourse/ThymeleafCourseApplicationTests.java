02ba436f9ed53a871e11d327ad6a7d3c384260de
bergony.bandeira <bergony.bandeira@quark.tec.br>
2022-07-23 09:23:58 -0300

    new commit

---
+++ src/test/java/bergony/ml/thymeleafcourse/ThymeleafCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package bergony.ml.thymeleafcourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymeleafCourseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
